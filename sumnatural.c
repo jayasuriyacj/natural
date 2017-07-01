3a3a4d9f2636271f0d21b10d30c1dc642a82c11d
jayasuriyacj <jayasuriyac070@gmail.com>
2017-07-01 14:03:34 +0530

    Rename natural.c to sumnatural.c

---
+++ sumnatural.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i,sum=0;
+clrscr();
+printf("\n enter positive integer");
+scanf("%d",&n);
+for(i=1;i<=n;++i)
+{
+sum+=i;
+}
+printf("\n sum=%d",sum);
+return 0;
+}
